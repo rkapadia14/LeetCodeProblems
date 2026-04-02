384c9a7f81d08edfcca590427753fe9748a9a09e
rkapadia14 <rinalikapadia1995@gmail.com>
2026-04-02 12:44:38 -0700

    Leetcode Problem 3 - Length of Longest Substring w/o rep chars - Med

---
+++ VariableSizeSlidingWindow/com/leetcode/slidingwindow/variable/LengthOfLongestSubString.java
@@ -0,0 +1,48 @@
+/*
+Leetcode Problem No: 3
+
+Leetcode Problem Name: Longest Substring Without Repeating Characters
+
+Problem Statement: Given a string s, find the length of the longest substring 
+without duplicate characters.
+
+Problem Type: Variable Size Sliding Window 
+
+Space Complexity: O(1)
+
+Time Complexity: O(n)
+
+Difficulty Level: Medium
+
+ */
+package com.leetcode.slidingwindow.variable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubString {
+	public static int lengthOfLongestSubstring(String s) {
+        Set<Character> hs = new HashSet<>();
+        int left=0;
+        int maxLen = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            while(hs.contains(s.charAt(i)))
+            {
+                hs.remove(s.charAt(left));
+                left++;
+            }
+            hs.add(s.charAt(i));
+            maxLen = Math.max(maxLen, hs.size());
+            
+        }
+        return maxLen;
+    }
+	public static void main(String[] args) {
+		System.out.println("Max length of substring without repeating characters for the string 'zxyzxyz' is "+lengthOfLongestSubstring("zxyzxyz"));
+		System.out.println("Max length of substring without repeating characters for the string 'xxxx' is "+lengthOfLongestSubstring("xxxx"));
+		System.out.println("Max length of substring without repeating characters for the string 'pwwkew' is "+lengthOfLongestSubstring("pwwkew"));
+
+	}
+
+}
